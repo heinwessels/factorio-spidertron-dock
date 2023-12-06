cd0f6bd515e416c194ac5d76756df4e537237785
Hein Wessels <heinwessels93@gmail.com>
2023-12-06 13:11:31 +0100

    Spiderbots no longer able to dock

--- registry.lua
+++ registry.lua
@@ -3,18 +3,22 @@ local registry = { }
 local black_list = {
     -- Space Exploration
     ["se-burbulator"] = true,
-    
+
     -- Companions
     ["companion"] = true,
-    
+
     -- Combat Robots Overhaul
     ["defender-unit"] = true,
     ["destroyer-unit"] = true,
-    
+
     -- Lex's Aircraft
     ["lex-flying-cargo"] = true,
     ["lex-flying-gunship"] = true,
     ["lex-flying-heavyship"] = true,
+
+    -- Spiderbots
+    ["spiderbot"] = true,
+
 }
 
 local black_list_regex = {
